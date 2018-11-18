a7c42bf32165df8d84b1de800297b0d18ee6a62b
egavilan1987 <enmanuelgc@gmail.com>
2018-11-18 06:49:28 -0400

    Almots done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// Empty JS for your own code to be here
+src="../js/jquery.min.js">
+src="../js/jquery.filterizr.js">
+// Sidenav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+// ScrollSpy
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+var filterizd = $('.filtr-container').filterizr({});
